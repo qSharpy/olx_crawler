7b8b1493e60ccc0dd67b71be846f168fe399ddad
Bogdan Bujor <bogdan.bujor08@gmail.com>
2019-05-09 10:56:14 +0300

    - SET AUTOTHROTTLE_TARGET_ CONCURRENCY = 30.0 5% mai multe date decat dezactivat

--- olx_crawler/settings.py
+++ olx_crawler/settings.py
@@ -80,7 +80,7 @@ CONCURRENT_REQUESTS = 4
 #AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 30.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
